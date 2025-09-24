5e715e85aedd89f6381121eef3da34f6c6f6be00
thisistoni <117242012+thisistoni@users.noreply.github.com>
2025-09-24 21:07:30 +0200

    Fix Welcome Message to Show Main Menu

--- src/bot/features/welcome.ts
+++ src/bot/features/welcome.ts
@@ -1,13 +1,16 @@
 import type { Context } from '#root/bot/context.js'
 import { logHandle } from '#root/bot/helpers/logging.js'
+import { createMainMenuKeyboard } from '#root/bot/keyboards/main-menu.js'
 import { Composer } from 'grammy'
 
 const composer = new Composer<Context>()
 
 const feature = composer.chatType('private')
 
-feature.command('start', logHandle('command-start'), (ctx) => {
-  return ctx.reply(ctx.t('welcome'))
+feature.command('start', logHandle('command-start'), async (ctx) => {
+  return ctx.reply(ctx.t('welcome'), {
+    reply_markup: await createMainMenuKeyboard(ctx),
+  })
 })
 
 export { composer as welcomeFeature }
